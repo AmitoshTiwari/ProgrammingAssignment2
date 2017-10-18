73ffbc64f5611d4ce36c55a1a4a151f39426e991
AmitoshTiwari <32902091+AmitoshTiwari@users.noreply.github.com>
2017-10-18 23:11:34 +0530

    summary
    updated function as per requireent of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makecachematrix  function returns a list which has 4 elements  
+## m is declared outside makecachematrix fuction so that inverse of the matrix is passed to it, it remains even after invocation of function
 
-## Write a short comment describing this function
 
+m <- NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+  set_matrix <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get_matrix <- function()x
+  
+  set_inverse_matrix <- function(inmat){m<<-inmat}
+  
+  get_inverse_matrix <- function()m
+  
+  list( get_matrix = get_matrix,
+        set_matrix = set_matrix,
+        set_inverse_matrix = set_inverse_matrix,
+        get_inverse_matrix = get_inverse_matrix)
+  
 }
 
 
-## Write a short comment describing this function
+## cachesolve function gets inverse of a matrix and display a message if it is reteived from cache 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m<-x$get_inverse_matrix()
+  if(!is.null(m))
+  {message("getting cached data")
+    return(m)
+  }
+  data<- x$get_matrix()
+  data1<- solve(data)
+  m<-x$set_inverse_matrix(data1)
+  m
+  
+  }
